46ed97f5ba8deeccacd16f3029cc67f8ff972cf7
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-05 15:48:05 -0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gamevid-angular-tailwind-forms-routing-rxjs-auth-wasm-ffmpeg-videojs-vercel';
+}
