0cc01697c264a07d77ef4d7265bab2945d3bcdee
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-07 14:09:41 -0800

    Add ContentQuery Decorator to QueryList tabs and loop through them

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, Query, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
+  constructor() {}
+
+  ngAfterContentInit(): void {
+    // this.tabs
+    // console.log(this.tabs);
+  }
 }
