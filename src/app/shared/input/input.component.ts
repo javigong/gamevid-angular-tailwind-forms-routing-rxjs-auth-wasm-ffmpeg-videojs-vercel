f76492fdf931ddd37d05e80847e987b37d62a8bc
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-08 08:53:59 -0800

    Add email validation to register form

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -4,9 +4,10 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent {
   @Input() control: FormControl = new FormControl();
-
+  @Input() type = 'text';
+  @Input() placeholder = '';
 }
