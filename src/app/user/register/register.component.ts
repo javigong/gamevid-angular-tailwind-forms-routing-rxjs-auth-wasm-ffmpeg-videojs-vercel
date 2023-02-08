685576cf5f2132a90943f685f7801dd59bb7f0df
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-08 10:16:12 -0800

    Add password validation using patterns to the register from

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -10,9 +10,18 @@ export class RegisterComponent {
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+      ),
+    ]),
+    confirm_password: new FormControl('', [Validators.required]),
     phoneNumber: new FormControl(''),
   });
 }
