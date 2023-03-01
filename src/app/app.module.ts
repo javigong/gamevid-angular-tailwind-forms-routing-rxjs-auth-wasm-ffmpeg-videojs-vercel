35f77854e0430e81ef8618bf2aded309f4df81ac
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-03-01 12:04:01 -0800

    Add VideoModule to App Module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
+import { VideoModule } from './video/video.module';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AboutComponent } from './about/about.component'
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    VideoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
