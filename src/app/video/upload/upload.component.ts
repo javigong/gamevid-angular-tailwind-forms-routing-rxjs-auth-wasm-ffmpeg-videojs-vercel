9e2d5e70f9d2e1969b13e8007b8969b13be32ab0
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-01 14:54:02 -0700

    Handle file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UploadComponent {
   isDragover = false;
+  file: File | null = null;
 
   storeFile($event: Event) {
     this.isDragover = false;
-    
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+
+    console.log(this.file);
   }
 }
