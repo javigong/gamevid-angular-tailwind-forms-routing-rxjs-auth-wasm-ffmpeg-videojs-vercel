082d463dc86361f6e105c3accfcc3c162eedeb71
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-01 11:22:31 -0700

    Register route upload with data

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,13 @@ const routes: Routes = [
     data: {
       authOnly: true,
     },
-  },
+  }, {
+    path: 'upload',
+    component: UploadComponent,
+    data: {
+      authOnly: true,
+    }
+  }
 ];
 
 @NgModule({
