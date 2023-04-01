a8f4d244c1b0e3f1813147b6220a4c5700b8193c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-01 14:34:36 -0700

    Block drop and dragover events at UploadComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 // import { ModalService } from '../services/modal.service';
-import { ModalComponent } from './modal/modal.component';
-import { TabComponent } from './tab/tab.component';
-import { TabsContainerComponent } from './tabs-container/tabs-container.component';
-import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { AlertComponent } from './alert/alert.component';
+import { EventBlockerDirective } from './directives/event-blocker.directive';
+import { InputComponent } from './input/input.component';
+import { ModalComponent } from './modal/modal.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent, AlertComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+    EventBlockerDirective,
+  ],
   imports: [CommonModule, ReactiveFormsModule, NgxMaskModule.forRoot()],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent, AlertComponent],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+    EventBlockerDirective,
+  ],
   // providers: [ModalService]
 })
 export class SharedModule {}
