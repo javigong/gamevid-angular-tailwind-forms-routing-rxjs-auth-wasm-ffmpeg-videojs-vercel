1e313e7c7114a27def8f25512610b753a37e954f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-01 10:59:55 -0700

    Retrieving Data outside of the outlet

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,11 +4,10 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { delay, map, filter } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { delay, filter, map, switchMap } from 'rxjs/operators';
 import IUser from '../models/user.model';
-import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +16,7 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>; // $ at the end identifies a property as an Observable
   public isAuthenticatedWithDelay$: Observable<boolean>;
+  private redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
@@ -27,9 +27,15 @@ export class AuthService {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
-    this.router.events.pipe(
-      filter(e => e instanceof NavigationEnd),
-    ).subscribe(console.log)
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map((e) => this.route.firstChild),
+        switchMap((route) => route?.data ?? of({}))
+      )
+      .subscribe((data) => {
+        this.redirect = data.authOnly ?? false;
+      });
   }
 
   public async createUser(userData: IUser) {
@@ -61,7 +67,9 @@ export class AuthService {
 
     await this.auth.signOut();
 
-    await this.router.navigateByUrl('/');
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
 
