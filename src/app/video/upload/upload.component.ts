ae2f131b1994f263d9904e9c3ba086812b602386
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-02 11:06:42 -0700

    Add timestamp to stored file data

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -98,6 +98,7 @@ export class UploadComponent implements OnDestroy {
             title: this.title.value,
             fileName: `${clipFileName}.mp4`,
             url,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
           };
 
           const clipDocRef = await this.clipService.createClip(clip);
