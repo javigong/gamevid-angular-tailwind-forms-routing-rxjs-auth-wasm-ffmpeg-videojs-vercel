f8860804874ef7f0804be28393097d4e606b8b2d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-02 11:33:28 -0700

    Add db query to get user videos

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -5,6 +5,9 @@ import {
   DocumentReference,
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +15,25 @@ import IClip from '../models/clip.model';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
     this.clipsCollection = this.db.collection('clips');
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data);
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user=>{
+        if(!user) {
+          return of([]);
+        }
+
+        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+
+        return query.get()
+      })
+    )
   }
 }
