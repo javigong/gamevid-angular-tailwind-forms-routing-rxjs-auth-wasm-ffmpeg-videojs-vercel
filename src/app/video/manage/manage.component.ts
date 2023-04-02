d22c0973f76887bbad6a4a64817a4fbd3e21c6d5
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-02 13:10:11 -0700

    Add edit modal

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -12,22 +13,27 @@ export class ManageComponent implements OnInit {
   videoOrder = '1';
   clips: IClip[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private clipService: ClipService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder = params.sort === '2' ? params.sort : '1';
     });
-    this.clipService.getUserClips().subscribe(docs => {
+    this.clipService.getUserClips().subscribe((docs) => {
       this.clips = [];
 
-      docs.forEach(doc => {
+      docs.forEach((doc) => {
         this.clips.push({
           docID: doc.id,
           ...doc.data(),
-        })
-      })
-    })
+        });
+      });
+    });
   }
 
   sort(event: Event) {
@@ -39,5 +45,11 @@ export class ManageComponent implements OnInit {
         sort: value,
       },
     });
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.modal.toggleModal('editClip')
   }
 }
--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -6,9 +6,10 @@ import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
 import { VideoRoutingModule } from './video-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [ManageComponent, UploadComponent],
+  declarations: [ManageComponent, UploadComponent, EditComponent],
   imports: [CommonModule, VideoRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class VideoModule {}
